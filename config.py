ab2e2f88d026fe83cb766e6ad98e9bb34b2aa002
dylan03650-pixel <dylan03650@gmail.com>
2026-08-22 08:49:10 +0100

    config.py

--- config.py
+++ config.py
@@ -4,15 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()]
+API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY", "6331ea9062a3230572625b1361f739fd")
 
-MONTHLY_PRICE = 9.99
-CURRENCY = "USD"
-REFERRAL_DISCOUNT_THRESHOLD = 30
-REFERRAL_DISCOUNT_PERCENT = 15
-
-API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY", "")
-
+# Top 30 Leagues (API-Football IDs)
 TOP_LEAGUES = {
     39: "Premier League",
     140: "La Liga",
@@ -24,8 +18,24 @@ TOP_LEAGUES = {
     144: "Belgian Pro League",
     203: "Süper Lig",
     71: "Brasileirão",
-    128: "Liga Profesional Argentina",
+    128: "Liga Profesional",
     253: "MLS",
-    2: "Champions League",
-    3: "Europa League",
+    179: "Scottish Premiership",
+    40: "Championship",
+    262: "Liga MX",
+    98: "J1 League",
+    113: "Super League Greece",
+    207: "Swiss Super League",
+    119: "Danish Superliga",
+    103: "Eliteserien",
+    106: "Ekstraklasa",
+    136: "Serie B",
+    141: "La Liga 2",
+    79: "2. Bundesliga",
+    62: "Ligue 2",
+    94: "Primeira Liga",
+    203: "Süper Lig",
+    71: "Brasileirão Série A",
+    128: "Argentine Liga",
+    253: "Major League Soccer",
 }
